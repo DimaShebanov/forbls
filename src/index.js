ec0b9feb112a136c2901a533f8d75232a98f6e76
DimaShebanov <dima.shebanov1997@gmail.com>
2018-10-15 22:20:42 +0300

    subconfig

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './components/application';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+
+const store = createStore(
+
+);
+
 
 
 ReactDOM.render(
---
+++ src/reducers.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+import { filterActions } from 'redux-ignore';
+import moduleName from './components/application/reducers';
+
+export default combineReducers();
